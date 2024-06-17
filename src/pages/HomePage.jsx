d494f194be3cfdd9287e7f5dec2d26e485f102a3
Talassa <dev.tech.marc@gmail.com>
2024-06-17 14:16:25 +0200

    added layout on page and some components to test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
+import { Breaker } from "../components/shared/Breaker"
+import { Button } from "../components/shared/Button"
 import { CvLayout } from "../layouts/CvLayout"
 
 export const HomePage = () => {
   return (
     <CvLayout>
-        
+        <Breaker>titulo de prueba</Breaker>
+        <Button>boton</Button>
     </CvLayout>
   )
 }
