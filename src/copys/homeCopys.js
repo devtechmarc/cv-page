0e28409541a68b23cacb6abadd60f9b357b0b7d7
Talassa <dev.tech.marc@gmail.com>
2024-06-17 14:12:56 +0200

    added new copys to teh page

---
+++ src/copys/homeCopys.js
@@ -0,0 +1,5 @@
+export const HomeTitle = 'Hola, soy Marc Fernández👋';
+export const HomeDescription = 'Amante de la programación y el deporte, siempre he estado metido detrás del muro trabajando de Backend developer con Java. Ahora, directo a full stack. Mis 6 años de experiencia me han preparado para adaptarme a las situaciones y peculiaridades de cada proyecto, así como a tirar del hilo en busca de la mejor solución. <><>><<<<En cada uno de mis proyectos me he dedicado al desarrollo de evolutivos y correctivos. Así como de hablar con el cliente sobre la opción más viable acorde a lo que necesitaba en cada momento.';
+export const HomeSubtitle = '{ Mi experiencia laboral }';
+export const HomeSubtitle2 = '{ Mis estudios }';
+export const HomeSubtitle3 = '{ Echa un vistazo a mis proyectos }';
